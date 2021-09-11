d843b108a09d88cad2c8b9acf585e525bddd2068
Ksyu <Ksyu.shapovalenko@mail.ru>
2021-09-11 20:52:35 +0300

    Initial commit

---
+++ Home_Work_3/Home_Work_3/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("МЕНЮ");
+                Console.WriteLine("Нажмите 0, чтобы выйти");
+                Console.WriteLine("Нажмите 1, чтобы просмотреть первую задачу");
+                Console.WriteLine("Нажмите 2, чтобы просмотреть вторую задачу");
+                Console.WriteLine("Нажмите 3, чтобы просмотреть третью задачу");
+                Console.WriteLine("================================================");
+                Console.WriteLine();
+                int taskNumber;
+                string a;
+                do
+                {
+                    Console.Write("Введите номер: ");
+                    a = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("================================================");
+                    Console.WriteLine();
+                }
+                while (int.TryParse(a, out taskNumber) == false);
+                switch (taskNumber)
+                {
+                    case 0:
+                        Console.WriteLine("Завершение работы программы");
+                        Console.ReadLine();
+                        return;
+                    case 1:
+                        Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Некорректный номер задачи. Повторите ввод.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+        
+
+        #region Task1
+        static void Task1()
+        { /*
+            а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.
+            б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+            в) Добавить диалог с использованием switch демонстрирующий работу класса.
+             */
+            Console.Clear();
+            Console.WriteLine("Для того, чтобы продемонстрировать работу структуры нажмите 1");
+            Console.WriteLine("Для того, чтобы продемонстрировать работу класса нажмите 2");
+            Console.Write("Введите номер: ");
+            string s = Console.ReadLine();
+            int taskNumber;
+            if (int.TryParse(s, out taskNumber))
+            {
+                switch (taskNumber)
+                {
+                    case 1:
+
+                        Console.Clear();
+                        Complex complex1;
+                        complex1.re = 2;
+                        complex1.im = -3;
+
+                        Complex complex2;
+                        complex2.re = 5;
+                        complex2.im = 8;
+
+                        Complex sum = complex1.Sum(complex2);
+                        Complex proiz = complex1.Proizv(complex2);
+                        Complex minus = complex1.Minus(complex2);
+
+                        Console.WriteLine($"Сумма комплексных чисел {complex1.ToString()} и {complex2.ToString()} равна {sum.ToString()}");
+                        Console.WriteLine($"Разность комплексных чисел {complex1.ToString()} и {complex2.ToString()} равна {minus.ToString()}");
+                        Console.WriteLine($"Произведение комплексных чисел {complex1.ToString()} и {complex2.ToString()} равно {proiz.ToString()}");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+
+                    case 2:
+                        Console.Clear();
+                        while (true)
+                        {
+
+                            Console.WriteLine("Для того, чтобы выйти нажмите 0");
+                            Console.WriteLine("Для того, чтобы сложить комплексные числа нажмите 1");
+                            Console.WriteLine("Для того, чтобы вычесть комплексные числа нажмите 2");
+                            Console.WriteLine("Для того, чтобы перемножить комплексные числа нажмите 3");
+                            string s1;
+                            int taskNumberB;
+                            bool flag;
+                            do
+                            {
+                                Console.Write("Введите номер: ");
+                                s1 = Console.ReadLine();
+                                flag = int.TryParse(s1, out taskNumberB);
+                            }
+                            while (!flag);
+
+                            Complex1 complex11 = new Complex1();
+                            complex11.re = 2;
+                            complex11.im = -3;
+
+                            Complex1 complex22 = new Complex1();
+                            complex22.re = 5;
+                            complex22.im = 8;
+
+
+                            switch (taskNumberB)
+                            {
+                                case 0:
+                                    Console.WriteLine("Завершение работы программы");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                    return;
+
+                                case 1:
+                                    Complex1 sum1 = complex11.Sum(complex22);
+                                    Console.WriteLine($"Сумма комплексных чисел {complex11.ToString()} и {complex22.ToString()} равна {sum1.ToString()}");
+                                    Console.ReadLine();
+                                    break;
+
+                                case 2:
+                                    Complex1 minus1 = complex11.Minus(complex22);
+                                    Console.WriteLine($"Разность комплексных чисел {complex11.ToString()} и {complex22.ToString()} равна {minus1.ToString()}");
+                                    Console.ReadLine();
+                                    break;
+
+                                case 3:
+                                    Complex1 proiz1 = complex11.Proizv(complex22);
+                                    Console.WriteLine($"Произведение комплексных чисел {complex11.ToString()} и {complex22.ToString()} равно {proiz1.ToString()}");
+
+                                    Console.ReadLine();
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Некорректный номер задачи. Повторите ввод.");
+                                    break;
+                            }
+                        }
+                        
+
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный номер задачи. Повторите ввод.");
+                            break;
+                        
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+            Console.Clear();
+        }
+        #endregion
+
+        #region Task2
+        static void Task2()
+        {
+            /*
+             С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). 
+             Требуется подсчитать сумму всех нечётных положительных чисел. 
+             Сами числа и сумму вывести на экран, используя tryParse.
+             */
+            double sum = 0;
+            double a;
+            string str;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Введите число");
+                str = Console.ReadLine();
+                flag=double.TryParse(str,out a);
+                if (flag)
+                {
+                    if (a % 2 != 0 && a > 0)
+                    {
+                        sum = sum + a;
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Это не число!");
+                    Console.WriteLine();
+                }
+            }
+            while (str != "0");
+            Console.WriteLine($"Cумма всех введенных нечётных положительных чисел равна {sum}");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        #endregion
+
+        #region Task3
+        static void Task3()
+        {
+            /*
+             *Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. 
+             Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+             Написать программу, демонстрирующую все разработанные элементы класса.
+             */
+
+        }
+        #endregion
+
+        #region Complex(Struct)
+
+        struct Complex
+        {
+            public double im;
+            public double re;
+
+            public Complex Sum(Complex x)
+            {
+                Complex y;
+                y.im = im + x.im;
+                y.re = re + x.re;
+                return y;
+            }
+            public Complex Minus(Complex x)
+            {
+                Complex y;
+                y.im = im - x.im;
+                y.re = re - x.re;
+                return y;
+            }
+            public Complex Proizv(Complex x)
+            {
+                Complex y;
+                y.re=re*x.re-im*x.im;
+                y.im = re * x.im + im * x.re;
+                return y;
+
+            }
+
+            public string ToString() 
+            {
+                return $"{re}+{im}i";
+            }
+        }
+
+
+
+        #endregion
+
+        #region Complex(Class)
+        class Complex1 
+        {
+            public double im;
+            public double re;
+
+
+            public Complex1 Sum(Complex1 x)
+            {
+                Complex1 y=new Complex1();
+                y.im = x.im + this.im;
+                y.re = x.re + this.re;
+                return y;
+            }
+            public Complex1 Minus(Complex1 x)
+            {
+                Complex1 y = new Complex1();
+                y.im = this.im- x.im;
+                y.re = this.re-x.re;
+                return y;
+            }
+            public Complex1 Proizv(Complex1 x)
+            {
+                Complex1 y = new Complex1();
+                y.re = this.re * x.re - this.im * x.im;
+                y.im = this.re * x.im + this.im * x.re;
+                return y;
+            }
+            public string ToString()
+            {
+                return $"{re}+{im}i";
+            }
+
+
+
+        }
+        #endregion
+    }
+}
---
+++ Home_work_2/Home_work_2/Program.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Home_work_2
+{
+    class Program
+    {
+        //Шаповаленко Ксения
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("МЕНЮ");
+                Console.WriteLine("Нажмите 0, чтобы выйти");
+                Console.WriteLine("Нажмите 1, чтобы просмотреть первую задачу");
+                Console.WriteLine("Нажмите 2, чтобы просмотреть вторую задачу");
+                Console.WriteLine("Нажмите 3, чтобы просмотреть третью задачу");
+                Console.WriteLine("Нажмите 4, чтобы просмотреть четвертую задачу");
+                Console.WriteLine("Нажмите 5, чтобы просмотреть пятую задачу");
+                Console.WriteLine("Нажмите 6, чтобы просмотреть шестую задачу");
+                Console.WriteLine("Нажмите 7, чтобы просмотреть седьмую задачу");
+                Console.WriteLine("================================================");
+                Console.WriteLine("");
+                Console.Write("Введите номер: ");
+                int taskNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.WriteLine("================================================");
+                Console.WriteLine("");
+                switch (taskNumber)
+                {
+                    case 0:
+                        Console.WriteLine("Завершение работы программы");
+                        Console.ReadLine();
+                        return;
+                    case 1:
+                        Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                    case 4:
+                        Task4();
+                        break;
+                    case 5:
+                        Task5();
+                        break;
+                    case 6:
+                        Task6();
+                        break;
+                    case 7:
+                        Task7();
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный номер задачи. Повторите ввод.");
+                        break;
+                }
+            }
+        }
+        #region Task_1
+        static void Task1() 
+        {
+            //Написать метод, возвращающий минимальное из трёх чисел.
+            
+
+            Console.WriteLine("Введите 3 числа");
+            Console.Write("Первое число: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Второе число: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Третье число: ");
+            double c = double.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine($"{a} - минимальное число");
+            }
+            else if (b < a && b < c)
+            {
+                Console.WriteLine($"{b} - минимальное число");
+            }
+            else if (c < a && c < b)
+            {
+                Console.WriteLine($"{c} - минимальное число");
+            }
+            else if (a == b && a < c)
+            {
+                Console.WriteLine($"{a} и {b} - минимальные числа");
+            }
+            else if (a == c && a < b)
+            {
+                Console.WriteLine($"{a} и {c} - минимальные числа");
+            }
+            else if (b == c && b < a)
+            {
+                Console.WriteLine($"{b} и {c} - минимальные числа");
+            }
+            else { Console.WriteLine($"{a},{b} и {c} - равные числа, поэтому минимального числа нет"); }
+            Console.ReadLine();
+            Console.Clear();
+        }
+        #endregion
+
+        #region Task_2
+        static void Task2()
+        {
+            //Написать метод подсчета количества цифр числа.
+            int count = 0;
+            Console.Write("Введите целое число: ");
+            int a = int.Parse(Console.ReadLine());
+            int a1 = a;
+            while (a != 0)
+            {
+                a = a / 10;
+                count++;
+            }
+            Console.WriteLine($"Количество цифр в числе {a1} равно {count}");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        #endregion
+
+        #region Task_3
+        static void Task3()
+        {
+            //С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+            int a;
+            int sum = 0;
+            Console.WriteLine("В задаче ведётся подсчет суммы всех нечетных положительных чисел (чтобы закончить ввод нажмите 0).");
+            do
+            {
+                Console.WriteLine("Введите число");
+                a = int.Parse(Console.ReadLine());
+                if (a % 2 != 0 && a > 0)
+                {
+                    sum = sum + a;
+                }
+            }
+            while (a != 0);
+            Console.WriteLine(sum != 0 ? ("Сумма нечетных положительных чисел равна " + sum) : ("Вы не ввели нечетные положительные числа"));
+            Console.ReadLine();
+            Console.Clear();
+        }
+        #endregion
+
+        #region Task_4
+        static void Task4()
+        {
+            /*
+              Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. 
+              На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+              Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, 
+              программа пропускает его дальше или не пропускает. 
+              С помощью цикла do while ограничить ввод пароля тремя попытками.
+             */
+            bool test;
+            int count = 0;
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                string passW = Console.ReadLine();
+                count++;
+                test = (login == "root" && passW == "GeekBrains") ? true : false;
+                if (test == false)
+                {
+                    Console.WriteLine($"Вы не прошли авторизацию! Количество оствашихся попыток: {3 - count}");
+                }
+                Console.WriteLine("");
+            }
+            while (test == false && count != 3);
+            Console.WriteLine(test == true ? "Вы прошли авторизацию!" : "Иссякло количество попыток!");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+        #endregion
+
+        #region Task_5
+        static void Task5()
+        {
+            /*
+             а) Написать программу, которая запрашивает массу и рост человека, 
+             вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+             б) Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+            */
+            Console.Write("Введите ваш рост в метрах: ");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите вашу массу в килограммах: ");
+            double m = double.Parse(Console.ReadLine());
+
+            double I = Math.Round(m / (h * h), 2);
+
+            if (I < 18.5)
+            {
+                Console.WriteLine($"Ваш индекс массы тела равен {I}. Наблюдается недостаток массы тела.");
+                Console.WriteLine("Вам необходимо набрать как минимум {0}кг", GetNewWeight(18.5, h, m));
+
+            }
+            else if (I <= 24.9)
+            {
+                Console.WriteLine($"Ваш индекс массы тела равен {I}. Это нормальный вес.");
+            }
+            else if (I <= 29.9)
+            {
+                Console.WriteLine($"Ваш индекс массы тела равен {I}. Наблюдается избытчный вес.");
+                Console.WriteLine("Вам необходимо скинуть как минимум {0}кг", GetNewWeight(24.9, h, m));
+            }
+            else
+            {
+                Console.WriteLine($"Ваш индекс массы тела равен {I}. Вы страдайте ожирением.");
+                Console.WriteLine("Вам необходимо скинуть как минимум {0}кг", GetNewWeight(24.9, h, m));
+            }
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+        #endregion
+
+        #region Task_6
+        static void Task6()
+        {
+            /*
+             Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+             «Хорошим» называется число, которое делится на сумму своих цифр. 
+             Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+             */
+            DateTime Stime = DateTime.Now;
+            int count = 0;
+            for (int i=1; i<=1000000000; i++) 
+            {
+                if (i % SumNumbers(i) == 0) 
+                {
+                    count++;
+                }
+            }
+            DateTime Ftime = DateTime.Now;
+            Console.WriteLine($"Количество «хороших» чисел в диапазоне от 1 до 1 000 000 000 равно {count}");
+            Console.WriteLine($"Время выполнения программы равно {Ftime-Stime}");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+        #endregion
+
+        #region Task_7
+        static void Task7()
+        {
+            /*
+             a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+             б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+             */
+            Console.WriteLine("Введите начальное число интервала");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите конечное число интервала");
+                int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Начальное число должно быть больше конечного!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Console.Write("Ряд: ");
+                Count(a, b);
+                Console.WriteLine("");
+                Console.WriteLine($"Сумма чисел ряда равна {Sum(a, b)}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+        #endregion
+
+        #region Other
+        static double GetNewWeight(double I0, double h, double m1) 
+        {
+            double m0 = I0 * h * h;
+            return Math.Round(Math.Abs(m0-m1), 2);
+        }
+        static int SumNumbers(int a)
+        {
+            if (a == 0)                                  
+                return 0;                             
+            else return SumNumbers(a / 10) + a % 10;
+        }
+        static void Count(int a, int b) 
+        {
+
+            if (a<=b)
+            {
+                Console.Write($"{a} ");
+                Count(a + 1, b);
+            }
+        }
+        static int Sum(int a, int b)
+        {
+            if (a > b) return 0;
+            else return Sum(a + 1, b)+a;
+        }
+        #endregion
+         
+    }
+}
